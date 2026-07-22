ae231d0683ec2747d480cf8bd06c292e56b75aa8
Onkar Ankush Jadhav <113853487+onkar0629@users.noreply.github.com>
2026-07-22 19:08:59 +0530

    Day_02_Python_Tokens_and_Variables

--- Python/Daily-Learning/Day_02_Python_Tokens_and_Variables/practice.py
+++ Python/Daily-Learning/Day_02_Python_Tokens_and_Variables/practice.py
@@ -10,6 +10,8 @@ Instructions:
 3. Follow Python naming conventions.
 4. Execute your code in PyCharm.
 """
+import math
+from unittest.loader import VALID_MODULE_NAME
 
 # ============================================================
 # Section 1 : Literals
@@ -18,15 +20,28 @@ Instructions:
 # Q1.
 # Create an integer literal and print it.
 
+a = 15
+print(a)
+
 # Q2.
 # Create a floating-point literal and print it.
+
+a = 15.567
+print(float(a))
 
 # Q3.
 # Store your name in a string variable and print it.
 
+a = "onkar Jadhav"
+print(a)
+
 # Q4.
 # Create a Boolean variable named is_student
 # and assign it an appropriate value.
+
+
+is_student = True
+print("Is Student", is_student)
 
 # Q5.
 # Create variables for:
@@ -35,6 +50,11 @@ Instructions:
 # - Your city
 # Print all three values.
 
+a = 22
+b = 173
+c = "Ahmednagar"
+
+print(a, b, c)
 
 # ============================================================
 # Section 2 : Variables
@@ -44,6 +64,10 @@ Instructions:
 # Create a variable named college
 # and store your college name.
 
+college = "Dy Patil university"
+print(college)
+
+
 # Q7.
 # Create variables:
 #
@@ -51,6 +75,10 @@ Instructions:
 # quantity = 4
 #
 # Print both variables.
+
+price = 250
+quantity = 4
+print(price, quantity)
 
 # Q8.
 # Create three variables:
@@ -61,14 +89,24 @@ Instructions:
 #
 # Store your full name and print it.
 
+first_name = "Onkar"
+last_name = "Jadhav"
+full_name = (first_name + " " + last_name)
+print(full_name)
+
 # Q9.
 # Create a variable named marks
 # Assign any value and print it.
+
+marks = 45
+print(marks)
 
 # Q10.
 # Change the value of marks
 # and print it again.
 
+marks = 55
+print(marks)
 
 # ============================================================
 # Section 3 : Assignment Operator
@@ -81,12 +119,21 @@ Instructions:
 #
 # Print both variables.
 
+a = 15
+b = a
+print(a , b)
+
 # Q12.
 #
 # x = 20
 # y = 30
 #
 # Create a third variable that stores the sum.
+
+x = 20
+y = 30
+z = x + y
+print(z)
 
 # Q13.
 #
@@ -96,6 +143,10 @@ Instructions:
 # width = 5
 #
 # Calculate the area of a rectangle.
+
+length = 12
+width = 5
+print("Area of rectangle :",length * width)
 
 # Q14.
 #
@@ -107,6 +158,9 @@ Instructions:
 #
 # Calculate the area of a circle.
 
+radius = 7
+PI = 3.14
+print("Area of circle :",PI * radius ** 2)
 
 # ============================================================
 # Section 4 : Arithmetic Operators
@@ -127,10 +181,23 @@ Instructions:
 # Modulus
 # Exponentiation
 
+num1 = 20
+num2 = 6
+
+print("Addition of two numbers :",num1 + num2)
+print("Subtraction of two numbers :",num1 - num2)
+print("Multiplication of two numbers :",num1 * num2)
+print("Division of two numbers :",num1 / num2)
+print("floor division of two numbers :",num1 // num2)
+print("Modulo of two numbers :",num1 % num2)
+print("exponentiation of two numbers :",num1 ** num2)
+
 # Q16.
 #
 # Find the remainder when
 # 125 is divided by 8.
+
+print(125 % 8)
 
 # Q17.
 #
@@ -138,11 +205,15 @@ Instructions:
 #
 # 100 // 9
 
+print(100 // 9)
+
 # Q18.
 #
 # Calculate:
 #
 # (15 + 5) * 2
+
+print((15 +5) * 2)
 
 # Q19.
 #
@@ -150,53 +221,54 @@ Instructions:
 #
 # 3 ** 4
 
+print(3 ** 4)
 
 # ============================================================
 # Section 5 : Identifiers
 # ============================================================
 
 # Write VALID or INVALID.
-
-# Q20.
 #
-# student_name
-
-# Q21.
-#
-# totalMarks
-
-# Q22.
-#
-# 2student
-
-# Q23.
-#
-# employee-id
-
-# Q24.
-#
-# _salary
-
-# Q25.
-#
-# class
-
-# Q26.
-#
-# employee2
-
-# Q27.
-#
-# first name
-
-# Q28.
-#
-# __count
-
-# Q29.
-#
-# while
-
+# # Q20.
+# #
+# # student_name
+# VALID_
+# # Q21.
+# #
+# # totalMarks
+# valid
+# # Q22.
+# #
+# # 2student
+# invalid
+# # Q23.
+# #
+# # employee-id
+# invalid
+# # Q24.
+# #
+# # _salary
+# valid
+# # Q25.
+# #
+# # class
+# invalid
+# # Q26.
+# #
+# # employee2
+# valid
+# # Q27.
+# #
+# # first name
+# invalid
+# # Q28.
+# #
+# # __count
+# valid
+# # Q29.
+# #
+# # while
+# invalid
 
 # ============================================================
 # Section 6 : Output Prediction
@@ -209,35 +281,35 @@ Instructions:
 print(10 + 20)
 
 # Output:
-# _______________________
+# 30
 
 # Q31.
 
 print(20 / 5)
 
 # Output:
-# _______________________
+# 4
 
 # Q32.
 
 print(20 // 6)
 
 # Output:
-# _______________________
+# 3
 
 # Q33.
 
 print(20 % 6)
 
 # Output:
-# _______________________
+# 2
 
 # Q34.
 
 print(2 ** 5)
 
 # Output:
-# _______________________
+# 32
 
 # Q35.
 
@@ -246,7 +318,7 @@ name = "Python"
 print(name)
 
 # Output:
-# _______________________
+# Python
 
 
 # ============================================================
