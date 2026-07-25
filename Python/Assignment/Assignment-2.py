c72eafe0c8f46c196d94936196163221a4585f7e
Onkar Ankush Jadhav <113853487+onkar0629@users.noreply.github.com>
2026-07-25 22:41:08 +0530

    Completed assigentment - 02

---
+++ Python/Assignment/Assignment-2.py
@@ -0,0 +1,40 @@
+# 1. W.a.p to check Data Quality, input for number of missing values incase if it
+# is 0 then print message as ‘cleaned Data’ otherwise print message as ‘Data Cleaning Required’
+
+# 2. W.a.p to check Data Storage, Read storage used, if it is above 80 then
+# display message as ‘Storage almost full’ otherwise display message as ‘Storage Okay’
+
+# 3. W.a.p to read username and password then check user is valid user or Invalid user
+# If user enters username as ‘AVD’ and password as ‘Python’ then valid user
+
+# 4. Consider customer has Rs:6000 in his account, now read withdraw amount
+# then update his balance. Note: if user enter valid amount then only update
+# balance otherwise display Error message as ‘Sorry...! You have low funds’
+
+# 5. W.a.p to read pizzacost, quantity. Now find billAmount, if billAmount is
+# above 5000 then provide 20% discount then find discount amount and final bill amount
+
+# 6. W.a.p to read employee annual salary then find monthly salary, if monthly
+# salary is 50000+ then 12% tax amount is deducted from monthly salary.
+# now find final monthly salary of an employee
+
+# 7. W.a.p to read age of a person, if age is between 18 to 24 then only they are
+# eligible to apply job otherwise display message as not eligible to apply job
+
+# 8. In Redbus, Read ticket cost and no.of tickets required then find billAmount.
+# Read coupon code from user, if they enter ‘save20’ then provide 20% discount
+
+# 9. W.a.p to read age of person, then check person has to take half ticket or full
+# ticket or no ticket
+# Incase age is <5 then ‘No Ticket’
+# Incase age is between 5 to 12 then ‘Half Ticket’
+# Otherwise need to take ‘Full Ticket’
+
+# 10.W.a.p to monitor time taken to finish a task. Read duration in minutes.
+# If it is 60+ minutes  Delay job
+# 30 – 60 minutes  Normal Job
+# Otherwise  Fast Job
+
+
+n =5
+print(5//2)
